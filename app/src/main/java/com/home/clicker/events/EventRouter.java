ea2ccc07c8523a9e2cfcebdded59e3a61fef4641
Константин <exslims1@mail.ru>
2016-12-09 16:06:09 +0400

    Fix NPE in EventRouter and added multiple handlers support

--- app/src/main/java/com/home/clicker/events/EventRouter.java
+++ app/src/main/java/com/home/clicker/events/EventRouter.java
@@ -1,6 +1,8 @@
 package com.home.clicker.events;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -8,11 +10,19 @@ import java.util.Map;
  * 08.12.2016
  */
 public class EventRouter {
-    private static final Map<Class, SCEventHandler> eventHandlerMap = new HashMap<Class, SCEventHandler>();
-    public static void fireEvent(SCEvent SCEvent){
-        eventHandlerMap.get(SCEvent.getClass()).handle(SCEvent);
+    private static final Map<Class, List<SCEventHandler>> eventHandlerMap = new HashMap<>();
+    public static void fireEvent(SCEvent event){
+        List<SCEventHandler> handlers = eventHandlerMap.get(event.getClass());
+        if(handlers != null) {
+            handlers.forEach(handler -> handler.handle(event));
+        }
     }
     public static void registerHandler(Class eventClass, SCEventHandler handler){
-        eventHandlerMap.put(eventClass,handler);
+        List<SCEventHandler> scEventHandlers = eventHandlerMap.get(eventClass);
+        if(scEventHandlers == null){
+            scEventHandlers = new ArrayList<>();
+            scEventHandlers.add(handler);
+        }
+        eventHandlerMap.put(eventClass,scEventHandlers);
     }
 }
