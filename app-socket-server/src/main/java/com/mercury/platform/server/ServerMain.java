d1a9e3bf3527484bc659f7b16ac6330cd4822bfe
Admin <qnx1000@gmail.com>
2017-01-02 14:16:49 +0300

    test commit

--- app-socket-server/src/main/java/com/mercury/platform/server/ServerMain.java
+++ app-socket-server/src/main/java/com/mercury/platform/server/ServerMain.java
@@ -3,7 +3,7 @@ package com.mercury.platform.server;
 /**
  * Created by Константин on 02.01.2017.
  */
-public class Main {
+public class ServerMain {
     public static void main(String[] args) {
         
     }
