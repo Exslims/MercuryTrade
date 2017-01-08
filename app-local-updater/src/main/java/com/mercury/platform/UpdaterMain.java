14c7384d1b48c009b1df3f2e6177264e04d220c7
Константин <exslims1@mail.ru>
2017-01-08 13:51:13 +0400

    Added app-local-updater module.

---
+++ app-local-updater/src/main/java/com/mercury/platform/UpdaterMain.java
@@ -0,0 +1,10 @@
+package com.mercury.platform;
+
+/**
+ * Created by Константин on 08.01.2017.
+ */
+public class UpdaterMain {
+    public static void main(String[] args) {
+        //todo
+    }
+}
