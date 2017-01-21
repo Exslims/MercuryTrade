5f5aaca7d6656effcecefd88022b1d0d79c34f7a
etofok <etofok@gmail.com>
2017-01-21 21:39:50 +0300

    New / better tutorial slides; Readme fixes

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Feedback and suggestions:
 
 ---
   
-**MercuryTrader** is a Path of Exile overlay platform written in Java. It's designed to facilitate all the possible trading interactions within the game providing a much better user experience for the players. The program went through countless design interactions and made by active Path of Exile players who use this tool themselves on a regular basis.
+**MercuryTrader** is an overlay application for Path of Exile written in Java. It's designed to facilitate all possible trading interactions in the game providing a much better user experience for the players. The program went through countless design interactions and made by active Path of Exile players who use this tool themselves on a regular basis.
 
 ---
 ## FUNCTIONALITY:
@@ -16,17 +16,17 @@ Feedback and suggestions:
 
 &nbsp; *Notification*
 
-When you receive a trade related message this notification panel pops up. It has all the information you might need to make a decision. It offers a set of fully customizable "quick response buttons" (such as "Sold" / "No" / "Wait 3m" etc, customizable) as well as buttons to invite, kick and tradewith the person who made an offer. If you've recieved multiple notifications before dismissing your current one, they will stack up like a deck of cards underneath each other and won't clutter the screen. Notification panels can be set up to fade out after X seconds (customizable).
+When you receive a trade related message this notification panel pops up. It has all the information you might need to make a decision. It offers a set of fully customizable "quick response buttons" (such as "Sold" / "No" / "Wait 3m" etc, customizable) as well as buttons to invite, kick and tradewith the person who made an offer. If you've received multiple notifications before dismissing your current one, they will stack up like a deck of cards underneath each other and won't clutter the screen. Notification panels can be set up to fade out after X seconds (customizable).
 
 
 &nbsp; *History*
 
-Each notification you receive is stored in a separate file by MercuryTrader. History panel allows you to access that file in a useful way to interact with the customers. Comes in handy if you've been afking for a while or you want to reach a person you traded with some time ago or in case your client has crashed (in-game chat will be cleared, your history will not).
+Each notification you receive is stored in a separate file by MercuryTrader. History allows you to access that file in a useful way to interact with the customers. Comes in handy if you've been afking for a while or you want to reach a person you traded with some time ago or in case your client has crashed - in-game chat will be cleared, your history will not.
 
 
 &nbsp; *SuperTrade mode*
 
-Forces notification panels to NOT stack like cards - all notification you receive will be displayed unfolded. This is useful if you need to manage a lot of trades with multiple people at once, i.e. exchanging currency or other commodities. This will clutter the screen with notifications, but all of them will be right in front of your eyes. The reasoning behind this functionality is because people come and go in no specific order.
+Forces notification panels to NOT stack like cards - all notification you receive will be displayed unfolded. This is useful if you need to manage a lot of trades with multiple people at once, i.e. exchanging currency or other commodities, because people come and go in no specific order. This will clutter the screen with notifications, but all of them will be right in front of your eyes.
 
 &nbsp; *Accessibility status*
 
@@ -54,7 +54,7 @@ Afterwards you should see these 2 new panels:
 
 * (1) **Do Not Disturb** mode button switch. Activate this mode if you don't want to receive notifications. The messages still get stored in your History allowing you to access them later. When DND is disabled you will be receiving notifications as usual.
 
-* (2) **SuperTrade** mode button switch. Activates / Deactivates SuperTrade Mode - forces each notification to NOT stack like cards underneath each other.
+* (2) **SuperTrade** mode button switch. Activates / Deactivates SuperTrade Mode - each consecutive notification will be displayed folded / unfolded.
 
 * (3) **History** . Open / Close the History panel.
 
--- app-ui/src/main/java/com/mercury/platform/ui/misc/note/NotesLoader.java
+++ app-ui/src/main/java/com/mercury/platform/ui/misc/note/NotesLoader.java
@@ -18,20 +18,39 @@ public class NotesLoader {
     private List<Note> testedNotes(){
         List<Note> notes = new ArrayList<>();
 
-        Note note1 = new Note("Overview","MercuryTrade is a Path of Exile overlay application. It's designed to facilitate all the possible trading interactions and within the game providing a much better user experience for the players. The program went through countless design interactions and made by active Path of Exile players who use this tool themselves on a regular basis.","app/app-icon.png",NoteLayout.VERTICAL);
+        Note note1 = new Note("Overview","MercuryTrade is an overlay application for Path of Exile. It's designed to facilitate all possible trading interactions in the game providing a much better user experience for the players. It also has some other minor features such as Timer and Chat Scanner. \n\nThe program went through countless design interactions and made by active Path of Exile players who use this tool themselves on a regular basis.","app/app-icon.png",NoteLayout.VERTICAL);
         notes.add(note1);
-        Note note2 = new Note("Messages","When you receive a trade related message this notification panel pops up. It has all the information you might need to make a decision.","notes/first/1.png",NoteLayout.VERTICAL);
-        notes.add(note2);
-        Note note3 = new Note("Task bar","###################","notes/first/2.png",NoteLayout.VERTICAL);
+
+        Note note3 = new Note("Notification (1/2)","When you receive a trade related message this notification panel pops up. It has all the information you might need to make a decision. Let's take a look.","notes/first/1.png",NoteLayout.VERTICAL);
         notes.add(note3);
-        Note note4 = new Note("History","Each notification you receive is stored by MercuryTrade. History panel allows you to access that file in a useful way to interact with the customers.","notes/first/3.png",NoteLayout.HORIZONTAL);
+        Note note4 = new Note("Notification (2/2)","Header: \nName, Age   {image:\"app/invite.png\"} Invite  {image:\"app/kick.png\"} Kick  {image:\"app/trade.png\"} Trade  {image:\"app/openChat.png\"} Message  {image:\"app/close.png\"} Dismiss \nItem name, Cost \nResponse buttons","notes/first/1.png",NoteLayout.VERTICAL);
         notes.add(note4);
-        Note note5 = new Note("Trade modes","Changing trade modes forces notification panels to NOT stack like cards - all notification you receive will be displayed unfolded.","notes/first/4.png",NoteLayout.VERTICAL);
+        Note note5 = new Note("Quick response buttons","As you can see, these are fully customizable. Select an already existing button (or create a new one) -> label it -> write in your message.","notes/first/5.png",NoteLayout.VERTICAL);
         notes.add(note5);
-        Note note6 = new Note("Quick response buttons","Select an existing button or create a new one -> label it -> write in your message.","notes/first/5.png",NoteLayout.VERTICAL);
+        Note note6 = new Note("History","Each notification you receive is stored by MercuryTrade in a separate file. You won't be losing trade offers due to client crashes anymore!","notes/first/3.png",NoteLayout.HORIZONTAL);
         notes.add(note6);
-        Note note7 = new Note("Other customization settings","Other settings todo","notes/first/6.png",NoteLayout.VERTICAL);
+        Note note7 = new Note("SuperTradeMode","By default (on the left) each consecutive notification you receive is folded to save screen space. SuperTradeMode is a switch that will force notification panels to be fully displayed. \nThis is useful if you need to manage a lot of trades with multiple people at once.","notes/first/4.png",NoteLayout.VERTICAL);
         notes.add(note7);
+        Note note8 = new Note("Accessibility status","MercuryTrade's notifications have an indication for when a person comes in or leaves your hideout - that way you can trade with him as soon as possible. Especially useful for SuperTradeMode.","",NoteLayout.VERTICAL);
+        notes.add(note8);
+        Note note9 = new Note("Do not Disturb","Activate this mode if you don't want to receive notification pop-ups for a while. The messages still get stored in your history allowing you to access them later.","",NoteLayout.VERTICAL);
+        notes.add(note9);
+        Note note10 = new Note("Taskbar","" +
+                "\n1) Do Not Disturb mode button switch. Activate this mode if you don't want to receive notifications." +
+                "\n2) SuperTrade mode button switch. Activates / Deactivates SuperTrade Mode - each consecutive notification will be displayed folded / unfolded" +
+                "\n3) Chat scanner. " +
+                "\n4) Timer. A simple timer with a few counters" +
+                "\n5) History. Open / Close your History panel" +
+                "\n6) Drag mode. Allows you to reposition panels on the screen." +
+                "\n7) Settings." +
+                "\n8) Exit.",
+                "notes/first/2.png",NoteLayout.VERTICAL);
+        notes.add(note10);
+
+
+
+        Note note11 = new Note("Other settings","Other settings todo","notes/first/6.png",NoteLayout.VERTICAL);
+        notes.add(note11);
         return notes;
     }
 }
