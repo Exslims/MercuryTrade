7f741524c1a22c93cfb509c852f7af18b4f21541
Admin <qnx1000@gmail.com>
2017-01-25 15:41:44 +0300

    Removed comments from ServerMain

--- app-socket-server/src/main/java/com/mercury/platform/server/main/ServerMain.java
+++ app-socket-server/src/main/java/com/mercury/platform/server/main/ServerMain.java
@@ -1,10 +1,7 @@
 package com.mercury.platform.server.main;
 
-import com.mercury.platform.bus.MessageBus;
 import com.mercury.platform.config.MercuryServerConfig;
 import com.mercury.platform.server.core.UpdaterServer;
-
-import java.net.InetSocketAddress;
 
 /**
  * Created by Frost on 02.01.2017.
@@ -12,10 +9,6 @@ import java.net.InetSocketAddress;
 public class ServerMain {
 
     public static void main(String[] args) throws Exception {
-
-//        MessageBus messageBus = MessageBus.getInstance();
-//        messageBus.fireUserConnectedEvent(new InetSocketAddress("127.0.0.1" , 5021));
-
         MercuryServerConfig serverConfig = MercuryServerConfig.getInstance();
         UpdaterServer server = new UpdaterServer(serverConfig.getPort());
         server.run();
