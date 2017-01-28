18498a2edde5dc276f985ece8d448e1dccec2c0f
Exslims <exslims1@mail.ru>
2017-01-28 15:39:28 +0400

    Added version field to server UI

--- app-local-updater/src/main/java/com/mercury/platform/UpdaterMain.java
+++ app-local-updater/src/main/java/com/mercury/platform/UpdaterMain.java
@@ -1,10 +1,13 @@
 package com.mercury.platform;
 
+import java.io.File;
+
 /**
  * Created by Константин on 08.01.2017.
  */
 public class UpdaterMain {
     public static void main(String[] args) {
-        //todo
+        File jar = new File("D:\\Test.txt");
+        System.out.println(jar.delete());
     }
 }
