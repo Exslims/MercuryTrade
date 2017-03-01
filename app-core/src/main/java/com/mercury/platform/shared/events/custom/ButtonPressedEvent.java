bcb59ab927a95cc0e0fe0ae8f2315e5a04477daa
Exslims <exslims1@mail.ru>
2017-03-01 08:01:54 +0400

    Added button pressed sound event.

---
+++ app-core/src/main/java/com/mercury/platform/shared/events/custom/ButtonPressedEvent.java
@@ -0,0 +1,9 @@
+package com.mercury.platform.shared.events.custom;
+
+import com.mercury.platform.shared.events.MercuryEvent;
+
+/**
+ * Created by Константин on 01.03.2017.
+ */
+public class ButtonPressedEvent implements MercuryEvent {
+}
