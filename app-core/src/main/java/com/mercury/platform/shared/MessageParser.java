bd1c07a5e8f76afdbc6467fc2ff1e3c0d157a2ad
Exslims <exslims1@mail.ru>
2017-03-25 22:46:45 +0400

    Fixed an issue when mercury not correctly show messages with float currency

--- app-core/src/main/java/com/mercury/platform/shared/MessageParser.java
+++ app-core/src/main/java/com/mercury/platform/shared/MessageParser.java
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class MessageParser {
-    private final static String poeTradePattern = "^(.*\\s)?(.+):.+ to buy your (.+) listed for (\\d+) (.+) in ([\\w\\-\\s]+)\\.*\\s*(\\(stash tab \"(.*)\"; position: left (\\d+), top (\\d+)\\))?\\s*(.*)$";
+    private final static String poeTradePattern = "^(.*\\s)?(.+):.+ to buy your (.+) listed for (\\d+(\\.\\d+)?)? (.+) in ([\\w\\-\\s]+)\\.*\\s*(\\(stash tab \"(.*)\"; position: left (\\d+), top (\\d+)\\))?\\s*(.*)$";
     private final static String poeCurrencyPattern = "^(.*\\s)?(.+):.+ to buy your (\\d+) (.+) for my (\\d+) (.+) in ([\\w\\-\\s]+)\\.*\\s*(.*)$";
     private final static String poeTradeNoBuyout = "^(.*\\s)?(.+):.+ to buy your (.+) in ([\\w\\-\\s]+)\\.*\\s*(\\(stash tab \"(.*)\"; position: left (\\d+), top (\\d+)\\))?\\s*(.*)$";
 
@@ -31,13 +31,13 @@ public class MessageParser {
             message.setWhisperNickname(poeTradeItemMatcher.group(2));
             message.setItemName(poeTradeItemMatcher.group(3));
             message.setCurCount(Double.parseDouble(poeTradeItemMatcher.group(4)));
-            message.setCurrency(poeTradeItemMatcher.group(5));
-            if(poeTradeItemMatcher.group(8) != null) {
-                message.setTabName(poeTradeItemMatcher.group(8));
-                message.setLeft(Integer.parseInt(poeTradeItemMatcher.group(9)));
-                message.setTop(Integer.parseInt(poeTradeItemMatcher.group(10)));
+            message.setCurrency(poeTradeItemMatcher.group(6));
+            if(poeTradeItemMatcher.group(9) != null) {
+                message.setTabName(poeTradeItemMatcher.group(9));
+                message.setLeft(Integer.parseInt(poeTradeItemMatcher.group(10)));
+                message.setTop(Integer.parseInt(poeTradeItemMatcher.group(11)));
             }
-            message.setOffer(poeTradeItemMatcher.group(11));
+            message.setOffer(poeTradeItemMatcher.group(12));
             return message;
         }
         Matcher poeTradeCurrencyMatcher = poeTradeCurrencyPattern.matcher(sourceMessage);
@@ -46,10 +46,10 @@ public class MessageParser {
             message.setSourceString(fullMessage);
             message.setWhisperNickname(poeTradeCurrencyMatcher.group(2));
             message.setCurrForSaleCount(Double.parseDouble(poeTradeCurrencyMatcher.group(3)));
-            message.setCurrForSaleTitle(poeTradeCurrencyMatcher.group(4));
-            message.setCurCount(Double.parseDouble(poeTradeCurrencyMatcher.group(5)));
-            message.setCurrency(poeTradeCurrencyMatcher.group(6));
-            message.setOffer(poeTradeCurrencyMatcher.group(8));
+            message.setCurrForSaleTitle(poeTradeCurrencyMatcher.group(5));
+            message.setCurCount(Double.parseDouble(poeTradeCurrencyMatcher.group(6)));
+            message.setCurrency(poeTradeCurrencyMatcher.group(8));
+            message.setOffer(poeTradeCurrencyMatcher.group(10));
             return message;
         }
         Matcher poeTradeNoBuyoutMatcher = poeTradeNoBuyoutPattern.matcher(sourceMessage);
