853cacb5588ea757b1a72f5e12bc6cae50bdd1a7
exslims <exslims1@mail.ru>
2017-10-25 03:29:52 +0400

    1.0.3.0.0 release commit

--- app-core/src/main/java/com/mercury/platform/core/MercuryConstants.java
+++ app-core/src/main/java/com/mercury/platform/core/MercuryConstants.java
@@ -1,7 +1,7 @@
 package com.mercury.platform.core;
 
 public class MercuryConstants {
-    public static final String APP_VERSION = "1.0.2.0.1";
+    public static final String APP_VERSION = "1.0.3.0.0";
     public static final String SERVER_HOST = "exslims.ddns.net";
     public static final int PORT = 5555;
 }
