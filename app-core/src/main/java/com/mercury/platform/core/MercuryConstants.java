4edb0750a9e39f3125f3dabf3b1c6bbdda88e23d
exslims <exslims1@mail.ru>
2020-01-06 14:38:43 +0400

    Update Immortal Syndicate help picture

--- app-core/src/main/java/com/mercury/platform/core/MercuryConstants.java
+++ app-core/src/main/java/com/mercury/platform/core/MercuryConstants.java
@@ -1,7 +1,7 @@
 package com.mercury.platform.core;
 
 public class MercuryConstants {
-    public static final String APP_VERSION = "1.0.3.4.0";
+    public static final String APP_VERSION = "1.0.3.6.0";
     public static final String SERVER_HOST = "exslims.ddns.net";
     public static final int PORT = 5555;
 }
